4dc9ba6ded93302ca5f21e6d15950f43b57c4e73
SAnatoly <Anatoly.Shapov@yandex.ru>
2018-06-06 10:39:08 +0300

    Test

---
+++ Assets/Scripts/Moove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moove : MonoBehaviour {
+
+    public float speed = 0.5f;
+    public float distance = 8;
+	void Start ()
+    {
+        Debug.Log(speed); // выводит в консоль надпись speed
+
+	}
+	
+	
+	void Update ()
+    {
+        if (transform.position.x >= distance || transform.position.x <= -distance) // финкция с функцией если if и ||(или)
+        {
+            speed = -speed;
+        }
+       /* else
+        {
+            transform.Translate(speed, 0, 0); // объект остановится если он выпоолнил действие
+        }8*/
+        //transform.Translate(3, 0, 0); // передвиает объект на определённое растояние
+        transform.Translate(speed * Time.deltaTime, 0, 0); // передвиает объект на определённое растояние
+    
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 0.7f;
+    public float rotSpeed = 100;
+    public float jumpSpeed = 300;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        
+            float movX = Input.GetAxis("Horizontal") * speed * Time.deltaTime; // получить ось с предусмотреннами за это клавишами
+            float movZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            float rotY = Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        float movY = 0;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            movY = jumpSpeed * Time.deltaTime;
+        }
+
+        transform.Translate(movX, movY, movZ); // перемещение объекта ( управоление колавишами )
+            transform.Rotate(0, rotY, 0); // поворот игрока
+        
+        
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy") // папроверяем объект столкновения на соответствие (если предмет коснулся объект с тегом Emeny)
+        {
+            Debug.Log("You die");
+            GetComponent<Renderer>().material.color = Color.red; // меняет цвет объекта
+            Time.timeScale = 0; // останавливает время
+        }
+    }
+}
