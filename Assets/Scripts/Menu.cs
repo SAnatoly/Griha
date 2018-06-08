677314ddc2190e9c04d1f345e2cd794815b9970d
SAnatoly <Anatoly.Shapov@yandex.ru>
2018-06-08 14:15:49 +0300

    Lux

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit(); // выход, работает только на компьютере
+    }
+}
